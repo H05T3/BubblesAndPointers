cff4194e2582615131cbf626d630225ec30da613
Humberto Torres <133621997+H05T3@users.noreply.github.com>
2025-01-17 16:08:03 +0000

    Finished product

--- lab.c
+++ lab.c
@@ -5,11 +5,11 @@ void printValues(int*);
 void sort(int*);
 void swap(int*,int*);
 
-int Main(){
+int main(){
 	int values[] = {7,3,9,4,6,1,2,8,5};
 	printf("Before: \n");
 	printValues(values);
-/*
+
 	//Test Swap
 	int x = 3;
 	int y = 5;
@@ -20,17 +20,17 @@ int Main(){
 	sort(values);
 	printf("After: \n");
 	printValues(values);
-*/
+
 	return(0);}
 	//End Main
 
 void printValues(int* array){
-	i = 0;
+	int i = 0;
 	printf("[");
 	for (i = 0; i < MAX; i++){
 		printf("%d, ",array[i]);
 	}
-	printf("]");
+	printf("]\n");
 }
 
 void swap(int* x,int* y){
@@ -40,14 +40,16 @@ void swap(int* x,int* y){
 }
 
 void sort(int* array){
-	i = 0;
-	j = 0;
+	int i = 0;
+	int j = 0;
 	for (i = 0; i <= MAX - 1; i++){
 		for(j = 0; j <= MAX - 1; j++){
-			if array[j] > array[j+1]{
+			if (array[j] > array[j+1]){
 				swap(&array[j],&array[j+1]);
 				printValues(array);
 			}
 		}
 	}
 }
+
+
